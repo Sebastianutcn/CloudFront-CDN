167b9738e676750f0c27c5610f245af9bcf0e2bf
Sebastian Adrian Babiciu <72445123+Sebastianutcn@users.noreply.github.com>
2024-09-24 10:09:42 +0300

    Add files via upload

---
+++ s3.tf
@@ -0,0 +1,62 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.s3_name
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
+  depends_on = [
+    aws_s3_bucket_ownership_controls.s3_bucket_ownership_controls, 
+    aws_s3_bucket_public_access_block.s3_bucket_public_access_block
+    ]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"           
+  source = "./src/index.html"   
+  # acl    = "public-read"          
+
+  content_type = "text/html"      
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"           
+  source = "./src/error.html"   
+  # acl    = "public-read"          
+
+  content_type = "text/html"      
+}
